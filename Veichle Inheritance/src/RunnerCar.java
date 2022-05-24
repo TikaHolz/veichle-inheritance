f717207fe62818c8e55df685cf086f7b8d4fd762
marika.holz <marika.holz@MU-C1-08.mullen.pvt.k12.co.us>
2022-05-24 13:08:45 -0600

    initial

---
+++ Veichle Inheritance/src/RunnerCar.java
@@ -0,0 +1,16 @@
+
+public class RunnerCar
+	{
+	public static void main(String[] args)
+		{
+		//Vehicle x = new Vehicle();
+		//Veichle x = new Bicycle();
+		Bicycle x = new Bicycle();
+				
+		x.displaySpeed();
+		x.hitPedestrian();
+		x.ringBell();
+	
+		}
+	
+	}
---
+++ Veichle Inheritance/src/Vehicle.java
@@ -0,0 +1,14 @@
+
+public abstract class Vehicle
+	{
+	//private String type = "Vehicle";
+	public abstract void displaySpeed();
+		//{
+		//System.out.println("not fast enough");
+		//}
+	public abstract void hitPedestrian();
+		{
+		System.out.println("Splat");
+		}
+
+	}
